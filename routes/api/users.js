eee815386a2982ebe71a16b6fea0ce60179f3d89
Alex Barke <alex.barke@csintelligence.asia>
2020-01-13 22:46:31 +0800

    in progress user creation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route       GET api/users
 // @desc        Register User
@@ -17,12 +21,42 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if the user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists." }] });
+      }
+      // get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // encrypt users password
+      // return json webtoken
+      console.log(req.body);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(`server error`);
+    }
+
     res.send(`user route`);
   }
 );
