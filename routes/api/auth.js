6b5190c75cfa5eb07be93d3f5db47c7251637375
Alex Barke <alex.barke@csintelligence.asia>
2020-01-14 21:24:47 +0800

    auth middleware, auth route returns user data without password

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+const auth = require("../../middleware/auth");
 
 // @route       GET api/auth
 // @desc        Test route
 // @access      Public
-router.get("/", (req, res) => res.send(`auth route`));
+router.get("/", auth, async (req, res) => {
+  try {
+    // get the user without the password
+    const user = await User.findById(req.user.id).select("-password");
+
+    // return the user
+    res.json(user);
+  } catch (err) {
+    // return a 500
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
 
 module.exports = router;
