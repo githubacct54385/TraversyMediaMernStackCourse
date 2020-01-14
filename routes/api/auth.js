ccd1ca8203daad544fce35b656e2cbe87faa8682
Alex Barke <alex.barke@csintelligence.asia>
2020-01-14 21:33:21 +0800

    in progress

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
 const auth = require("../../middleware/auth");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 // @route       GET api/auth
 // @desc        Test route
@@ -19,5 +23,57 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("server error");
   }
 });
+
+// @route       GET api/auth
+// @desc        Authenticate user & get token
+// @access      Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // see if the user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials." }] });
+      }
+
+      // in progress...
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(`server error`);
+    }
+  }
+);
 
 module.exports = router;
