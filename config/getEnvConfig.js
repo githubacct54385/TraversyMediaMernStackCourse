b58be6d89084e478d875256b4361a2441d32b9c1
Alex Barke <alex.barke@csintelligence.asia>
2020-03-07 14:39:53 +0800

    Update getEnvConfig.js

--- config/getEnvConfig.js
+++ config/getEnvConfig.js
@@ -2,17 +2,23 @@ const config = require("config");
 
 function getEnvConfig(configName) {
   // check runtime
-  console.log("Printing node env...");
-  console.log(process.env.NODE_ENV);
   const runtime = process.env.NODE_ENV;
   if (runtime === "production") {
     // get from heroku env variables
-    console.log(`getting ${configName} from production`);
-    return process.env.configName;
+    return getProductionConfig(configName);
   } else {
     // get from local config
-    console.log(`getting ${configName} from development`);
     return config.get(configName);
+  }
+}
+
+function getProductionConfig(configName) {
+  if (configName === null || configName === undefined || configName === "") {
+    throw "Production Config Name cannot be null or undefined or an empty string.";
+  }
+  switch (configName) {
+    case "mongoURI":
+      return process.env.MongoUri;
   }
 }
 
